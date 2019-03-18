acd1b345753452abf0d69696c6831b2b82745526
lt843765372 <843765372@qq.com>
2019-03-18 10:07:19 +0800

    添加vue项目
    加项目

---
+++ firstproject/firpro/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ hi }}</h1>
+    <h1>{{ msg }}</h1>
+    <router-link to="./skip">skip</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to HOME',
+      hi: 'hi,小笨蛋'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ firstproject/firpro/src/components/Skip.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      skip
+    </div>
+</template>
+
+<script>
+    export default {name: 'Skip'}
+</script>
+
+<style scoped>
+
+</style>
